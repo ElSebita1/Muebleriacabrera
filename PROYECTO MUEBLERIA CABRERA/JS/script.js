854a7590a18216c1902e6213c4c5b1f1a2b4565f
ElSebita1 <135097152+ElSebita1@users.noreply.github.com>
2023-06-09 01:35:00 -0400

    Add files via upload

---
+++ PROYECTO MUEBLERIA CABRERA/JS/script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('#menu-icon')
+let navegador = document.querySelector('.navegador')
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navegador.classList.toggle('open');
+};
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset:true
+});
+sr.reveal('.monitoinicio-texto', {delay:200, origin:'left'})
+sr.reveal('.monitoinicio-img', {delay:450, origin:'top'})
+sr.reveal('.volver-arriba', {delay:500, origin:'right'})
+sr.reveal('.muestrario', {delay:200, origin:'bottom'})
+sr.reveal('.variedad', {delay:200, origin:'top'})
+sr.reveal('.textoinicio', {delay:200, origin:'right'})
+sr.reveal('.caja-prin', {delay:300, origin:'bottom'})
